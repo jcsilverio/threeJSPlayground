fa18edc7df570b534e8a4e58bbe5832ba2e1ce98
JC Silverio <silverio@impulsevfx.com>
2017-12-07 21:22:14 -0500

    camera rig and animation added

--- main.js
+++ main.js
@@ -9,10 +9,10 @@ function init() {
     scene.fog = new THREE.FogExp2(0xffffff, 0.2);
   }
 
-  var plane = getPlane(30);
+  var plane = getPlane(100);
   var directionalLight = getDirectionalLight(1);
   var sphere = getSphere(0.05);
-  var boxGrid = getBoxGrid(10, 1.5);
+  var boxGrid = getBoxGrid(20, 2.5);
   boxGrid.name = 'boxGrid';
 
 
@@ -36,20 +36,34 @@ function init() {
     1000
   );
 
-
+var cameraZRotation = new THREE.Group();
+var cameraYPosition = new THREE.Group();
 var cameraZPosition = new THREE.Group();
 var cameraXRotation = new THREE.Group();
 var cameraYRotation = new THREE.Group();
 
+cameraZRotation.name = 'cameraZRotation';
+cameraYPosition.name = 'cameraYPosition';
+cameraZPosition.name = 'cameraZPosition';
+cameraXRotation.name = 'cameraXRotation';
+cameraYRotation.name = 'cameraYRotation';
 
-cameraZPosition.add(camera);  //z position controller
+cameraZRotation.add(camera)
+cameraYPosition.add(cameraZRotation)
+cameraZPosition.add(cameraYPosition);  //z position controller
 cameraXRotation.add(cameraZPosition);
 cameraYRotation.add(cameraXRotation);
 scene.add(cameraYRotation);
 
+cameraXRotation.rotation.x = -Math.PI/2;
+cameraYPosition.position.y = 1;
+cameraZPosition.position.z = 100;
+
  gui.add(cameraZPosition.position, 'z', 0, 100);
  gui.add(cameraYRotation.rotation, 'y', -Math.PI, Math.PI);  //using PI, because rotation doesn't work with angles in THREE.js, but with radians.
  gui.add(cameraXRotation.rotation, 'x', -Math.PI, Math.PI);
+ gui.add(cameraZRotation.rotation, 'z', -Math.PI, Math.PI);
+
 
   var renderer = new THREE.WebGLRenderer();
   renderer.shadowMap.enabled = true;
@@ -82,12 +96,12 @@ function getBoxGrid(amount, separationMultiplier) {
   var group = new THREE.Group();
 
   for (var i = 0; i < amount; i++) {
-    var obj = getBox(1, 1, 1);
+    var obj = getBox(1, 3, 1);
     obj.position.x = i * separationMultiplier;
     obj.position.y = obj.geometry.parameters.height / 2;
     group.add(obj);
     for (var j = 1; j < amount; j++) {
-      var obj = getBox(1, 1, 1);
+      var obj = getBox(1, 3, 1);
       obj.position.x = i * separationMultiplier;
       obj.position.y = obj.geometry.parameters.height / 2;
       obj.position.z = j * separationMultiplier;
@@ -151,10 +165,14 @@ function getDirectionalLight(intensity) {
   var light = new THREE.DirectionalLight(0xffffff, intensity);
   light.castShadow = true;
 
-  light.shadow.camera.left = -10;
-  light.shadow.camera.bottom = -10;
-  light.shadow.camera.right = 10;
-  light.shadow.camera.top = 10;
+  light.shadow.camera.left = -40;
+  light.shadow.camera.bottom = -40;
+  light.shadow.camera.right = 40;
+  light.shadow.camera.top = 40;
+
+  light.shadow.mapSize.width = 4096;
+  light.shadow.mapSize.height = 4096;
+
 
   return light;
 }
@@ -164,7 +182,21 @@ function update(renderer, scene, camera, controls, clock) {
     scene,
     camera
   );
+
+  controls.update();
+
   var timeElapsed = clock.getElapsedTime();
+
+  var cameraXRotation = scene.getObjectByName('cameraXRotation');
+  if (cameraXRotation.rotation.x < 0) {
+    cameraXRotation.rotation.x += 0.01;
+  }
+
+  var cameraZPosition = scene.getObjectByName('cameraZPosition');
+  cameraZPosition.position.z -= 0.25;
+
+  var cameraZRotation = scene.getObjectByName('cameraZRotation');
+  cameraZRotation.rotation.z = noise.simplex2(timeElapsed * 1.5, timeElapsed * 1.5) * 0.02;
 
   var boxGrid = scene.getObjectByName('boxGrid');
   boxGrid.children.forEach(function(child, index) {
@@ -175,7 +207,7 @@ function update(renderer, scene, camera, controls, clock) {
 
   })
 
-  controls.update();
+
 
 
 
